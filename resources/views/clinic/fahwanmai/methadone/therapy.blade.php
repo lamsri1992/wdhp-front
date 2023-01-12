0e34d83662c6e16be2ed16eac8f75d214b121413
satiya799 <satiya799@satiya799s-MacBook-Air.local>
2023-01-12 22:38:03 +0700

    add ccpi

--- resources/views/clinic/fahwanmai/methadone/therapy.blade.php
+++ resources/views/clinic/fahwanmai/methadone/therapy.blade.php
@@ -17,7 +17,7 @@
                 <div class="card-body">
                     <h5 class="card-title">
                         <i class="fa-solid fa-clock-rotate-left"></i>
-                        ข้อมูลประวัติการรับเข้าบำบัด : {{ $patient->patient_name }}
+                        ข้อมูลประวัติการเข้ารับบำบัด : {{ $patient->patient_name }}
                         <input id="hn" name="hn" type="hidden" value="{{ $patient->patient_hn }}">
                         <input id="id" name="id" type="hidden" value="{{ $patient->patient_id }}">
                     </h5>
@@ -30,43 +30,55 @@
                         <div class="col-md-9">
                             <nav>
                                 <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                                    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab"
-                                        data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home"
+                                    <button class="nav-link active" id="nav-ccpi-tab" data-bs-toggle="tab"
+                                        data-bs-target="#nav-ccpi" type="button" role="tab" aria-controls="nav-ccpi"
                                         aria-selected="true">
                                         <i class="fa-solid fa-heart-pulse"></i>
                                         ประวัติการเจ็บป่วย
                                     </button>
-                                    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab"
-                                        data-bs-target="#nav-profile" type="button" role="tab"
-                                        aria-controls="nav-profile" aria-selected="false">
+                                    <button class="nav-link" id="nav-order-tab" data-bs-toggle="tab"
+                                        data-bs-target="#nav-order" type="button" role="tab"
+                                        aria-controls="nav-order" aria-selected="false">
                                         <i class="fa-solid fa-prescription-bottle-medical"></i>
                                         รายการยา
                                     </button>
-                                    <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab"
-                                        data-bs-target="#nav-contact" type="button" role="tab"
-                                        aria-controls="nav-contact" aria-selected="false">
+                                    <button class="nav-link" id="nav-lab-tab" data-bs-toggle="tab"
+                                        data-bs-target="#nav-lab" type="button" role="tab"
+                                        aria-controls="nav-lab" aria-selected="false">
                                         <i class="fa-solid fa-flask-vial"></i>
                                         ผล LAB
+                                    </button>
+                                    <button class="nav-link" id="nav-telehealth-tab" data-bs-toggle="tab"
+                                        data-bs-target="#nav-telehealth" type="button" role="tab"
+                                        aria-controls="nav-telehealth" aria-selected="false">
+                                        <i class="fa-solid fa-video"></i>
+                                        Tele-Health
                                     </button>
                                 </div>
                             </nav>
                             <div class="tab-content" id="nav-tabContent">
-                                <div class="tab-pane fade show active" id="nav-home" role="tabpanel"
-                                    aria-labelledby="nav-home-tab">
+                                <div class="tab-pane fade show active" id="nav-ccpi" role="tabpanel"
+                                    aria-labelledby="nav-ccpi-tab">
                                     <div class="card-body" style="margin-top: 1rem;">
-                                        ประวัติการเจ็บป่วย CCPI
+                                        <div class="ccpi"></div>
                                     </div>
                                 </div>
-                                <div class="tab-pane fade" id="nav-profile" role="tabpanel"
-                                    aria-labelledby="nav-profile-tab">
+                                <div class="tab-pane fade" id="nav-order" role="tabpanel"
+                                    aria-labelledby="nav-order-tab">
                                     <div class="card-body" style="margin-top: 1rem;">
                                         รายการยา Drug Order
                                     </div>
                                 </div>
-                                <div class="tab-pane fade" id="nav-contact" role="tabpanel"
-                                    aria-labelledby="nav-contact-tab">
+                                <div class="tab-pane fade" id="nav-lab" role="tabpanel"
+                                    aria-labelledby="nav-lab-tab">
                                     <div class="card-body" style="margin-top: 1rem;">
                                         รายการผล LAB - Urine
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade" id="nav-telehealth" role="tabpanel"
+                                    aria-labelledby="nav-telehealth-tab">
+                                    <div class="card-body" style="margin-top: 1rem;">
+                                        Tele-Health
                                     </div>
                                 </div>
                             </div>
@@ -107,19 +119,88 @@
 
                     var row =
                         $(
-                            '<a href="#" class="list-group-item list-group-item-action">' +
+                            '<button role="button" class="list-group-item list-group-item-action" onclick="vnClick(this)"' + 
+                            ' data-id="'+ data[i].vn +'">' +
                                 '<div class="d-flex w-100 justify-content-between">' +
                                     '<h5 class="mb-1"><i class="fa-regular fa-calendar"></i> '+ vstdate +'</h5>' +
                                     '<small class="text-muted">dx F11.2</small>' +
                                 '</div>' +
                                 '<p class="mb-1">VN : '+ data[i].vn +'</p>' +
                                 '<small>ดูรายละเอียดการตรวจรักษา</small>' +
-                            '</a>'
+                            '</button>'
                         );
                     $('.list-thp').append(row);
                 }
             },
         });
     });
+
+    function vnClick(btn) {
+        var id = btn.getAttribute("data-id");
+        $.ajax({
+            url: "http://127.0.0.1:3000/vst/" + id,
+            success: function (data) {
+                $('.ccpi').html("");
+                var row =
+                    $(
+                    '<span class="badge bg-danger" style="font-size:16px;margin-bottom:0.5rem;">Vital Sign</span>' +
+                    '<div class="row">' +
+                        '<div class="col-md-6">' +
+                            '<ul class="list-group">' +
+                                '<li class="list-group-item d-flex justify-content-between align-items-center">' +
+                                    'BPD' +
+                                    '<span style="font-weight:bold;">'+ data[0].bpd +'</span>' +
+                                '</li>' +
+                                '<li class="list-group-item d-flex justify-content-between align-items-center">' +
+                                    'BPS' +
+                                    '<span style="font-weight:bold;">'+ data[0].bps +'</span>' +
+                                '</li>' +
+                                '<li class="list-group-item d-flex justify-content-between align-items-center">' +
+                                    'PULSE' +
+                                    '<span style="font-weight:bold;">'+ data[0].pulse +'</span>' +
+                                '</li>' +
+                                '<li class="list-group-item d-flex justify-content-between align-items-center">' +
+                                    'RR' +
+                                    '<span style="font-weight:bold;">'+ data[0].rr +'</span>' +
+                                '</li>' +
+                            '</ul>' +
+                        '</div>' + 
+                        '<div class="col-md-6">' +
+                            '<ul class="list-group">' +
+                                '<li class="list-group-item d-flex justify-content-between align-items-center">' +
+                                    'TEMPERATURE' +
+                                    '<span style="font-weight:bold;">'+ data[0].temperature +'</span>' +
+                                '</li>' +
+                                '<li class="list-group-item d-flex justify-content-between align-items-center">' +
+                                    'BW' +
+                                    '<span style="font-weight:bold;">'+ data[0].bw +'</span>' +
+                                '</li>' +
+                                '<li class="list-group-item d-flex justify-content-between align-items-center">' +
+                                    'HEIGHT' +
+                                    '<span style="font-weight:bold;">'+ data[0].height +'</span>' +
+                                '</li>' +
+                                '<li class="list-group-item d-flex justify-content-between align-items-center">' +
+                                    'BMI' +
+                                    '<span style="font-weight:bold;">'+ data[0].bmi +'</span>' +
+                                '</li>' +
+                            '</ul>' +
+                        '</div>' +
+                    '</div>' +
+                    '<br>' +
+                    '<span class="badge bg-danger" style="font-size:16px;margin-bottom:0.5rem;">CCPI</span>' +
+                    '<div class="row">' +
+                        '<div class="col-md-12">' +
+                            '<ul class="list-group">' +
+                                '<li class="list-group-item d-flex justify-content-between align-items-center">' +
+                                    '<span style="font-weight:bold;">'+ data[0].cc +'</span>' +
+                                '</li>' +
+                            '</ul>' +
+                        '</div>' +
+                    '</div>'
+                    );
+                $('.ccpi').append(row);
+            },
+        });
+    }
 </script>
 @endsection
