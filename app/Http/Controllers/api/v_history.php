c413a980dfffe49ad8cc0121be84d549f7c0d818
lamsri1992 <satiya3577@gmail.com>
2023-06-19 10:46:20 +0700

    fix bug v_history api

--- app/Http/Controllers/api/v_history.php
+++ app/Http/Controllers/api/v_history.php
@@ -10,13 +10,25 @@ class v_history extends Controller
 {
     public function show($id)
     {
-        $patient = DB::table('h_patient')->where('idcard',$id)->first();
-        $history = DB::table('h_visit')
-                ->select('visitdate','visitno','pid','h_code','h_name','h_color')
-                ->where('pid',$patient->pid)
-                ->join('hos','hos.h_code','h_visit.pcucode')
-                ->orderby('visitdate','desc')
+        $patient = DB::table('h_patient')
+                ->where('idcard',$id)
+                ->first();
+        $hospital = DB::table('h_patient')
+                ->select('pcucodeperson')
+                ->where('idcard',$id)
                 ->get();
+        $newArray = $hospital->map(function($item){
+            return $item->pcucodeperson;
+        })->toArray();
+        $in_array = "'".implode("','",$newArray)."'";
+        $history = DB::select(DB::raw(
+           "SELECT h_visit.visitdate, h_visit.visitno, h_visit.pid, idcard, hcode, h_code, h_name, h_color 
+            FROM h_visit 
+            LEFT JOIN h_patient ON h_patient.pid = h_visit.pid 
+            LEFT JOIN hos ON hos.h_code = h_visit.pcucode 
+            WHERE h_visit.pid = '$patient->pid'
+            AND h_visit.pcucode IN ($in_array) 
+            ORDER BY h_visit.visitdate DESC"));
         return response()->json($history);
     }
 }
